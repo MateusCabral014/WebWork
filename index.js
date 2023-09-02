5df9343799da1be2e402ae2052fa7136b700c8bd
Mateus Cabral da Silva <131392438+MateusCabralSilva@users.noreply.github.com>
2023-09-02 17:20:04 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var menuBtn = document.querySelector(".menu-btn");
-    var menu = document.querySelector(".menu");
-    var overlay = document.querySelector(".overlay");
-    menuBtn.classList.toggle("open");
-    menu.classList.toggle("open");
-    overlay.classList.toggle("open");
-}
